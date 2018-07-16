35f146b9962e2a39b3a6aef1c9a559f56fc65172
Will Owens <malmoi112@gmail.com>
2018-07-16 08:45:09 -0400

    more initialisation

---
+++ malnet-client/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+import './App.css';
+
+import Home from './Home/Home';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Route exact path="/" component={Home} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ malnet-client/src/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = (props) => {
+  return (
+    <div className="Home">
+      <header>Home</header>
+    </div>
+  );
+}
+
+export default Home;
